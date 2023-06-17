8ea56a6a31f0aff79ef331a82bf874fcbcfcd85b
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-06-17 19:12:13 +0530

    Added tf

---
+++ May23/workshop/playbook/ec2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "gol" {
+  ami             = "ami-0c65adc9a5c1b5d7c"
+  instance_type   = "t2.micro"
+  tags = {
+    Name = "gol"
+  }
+}
+output "gol_url" {
+  value = format("%s ansible_ssh_private_key_file=/home/ansible/tfansible.pem ansible_ssh_user=ubuntu",aws_instance.gol.public_ip)
+}
